0753f81bf770482845d0a5fc3d43788c264877b1
g3rar2 <>
2025-10-16 20:37:24 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ceutec-app',
+  webDir: 'www'
+};
+
+export default config;
