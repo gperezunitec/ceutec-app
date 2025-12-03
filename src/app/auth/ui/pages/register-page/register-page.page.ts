801123cb6c39c3a50e2ea02f7019ee228a54beda
g3rar2 <>
2025-12-02 20:20:49 -0600

    Crear Cuenta Form

--- src/app/auth/ui/pages/register-page/register-page.page.ts
+++ src/app/auth/ui/pages/register-page/register-page.page.ts
@@ -1,20 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {
+  IonButton,
+  IonCheckbox,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
+import {IonicModule} from "@ionic/angular";
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.page.html',
   styleUrls: ['./register-page.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [ CommonModule, FormsModule, IonicModule, ReactiveFormsModule]
 })
-export class RegisterPagePage implements OnInit {
+export class RegisterPagePage  {
 
-  constructor() { }
+  private readonly formBuilder:FormBuilder=inject(FormBuilder);
+  registerForm:FormGroup=this.formBuilder.group({
+    name:['', Validators.required],
+    email:['', [Validators.required, Validators.email]],
+    password:['', [Validators.required]],
+    phone:['', Validators.required],
+  });
 
-  ngOnInit() {
+
+  get isEmailRequired():boolean{
+    const emailControl=this.registerForm.get('email');
+    return emailControl?emailControl.hasError('required')&& emailControl.touched:false;
+  }
+
+
+  get isEmailInvalid():boolean{
+    const emailControl=this.registerForm.get('email');
+    return emailControl?emailControl.hasError('email')&& emailControl.touched:false;
+  }
+
+
+  get isPasswordRequired():boolean{
+    const passwordControl=this.registerForm.get('password');
+    return passwordControl?passwordControl.hasError('required')&& passwordControl.touched:false;
+  }
+
+
+  get isFormInvalid():boolean{
+    return this.registerForm.invalid;
+  }
+
+  onSubmit():void {
+
   }
 
 }
